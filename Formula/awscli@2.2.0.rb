f6f311aaa4ab95679234e8aa156147510ce05e78
Mihir Pathak <8856969+mihirpathak97@users.noreply.github.com>
2021-07-30 13:00:42 +0530

    add awscli@2.2.0

---
+++ Formula/awscli@2.2.0.rb
@@ -0,0 +1,56 @@
+class AwscliAT220 < Formula
+  include Language::Python::Virtualenv
+
+  desc "Official Amazon AWS command-line interface"
+  homepage "https://aws.amazon.com/cli/"
+  url "https://github.com/aws/aws-cli/archive/2.2.0.tar.gz"
+  sha256 "cef019bd932570362cb410c1788382f77d56180626c54364914e8ae016273842"
+  license "Apache-2.0"
+  head "https://github.com/aws/aws-cli.git", branch: "v2"
+
+  depends_on "cmake" => :build
+  depends_on "python@3.9"
+
+  uses_from_macos "groff"
+
+  on_linux do
+    depends_on "libyaml"
+  end
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    system libexec/"bin/pip", "install", "-v", "-r", "requirements.txt",
+                              "--ignore-installed", buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "awscli"
+    venv.pip_install_and_link buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "pyinstaller"
+    pkgshare.install "awscli/examples"
+
+    rm Dir["#{bin}/{aws.cmd,aws_bash_completer,aws_zsh_completer.sh}"]
+    bash_completion.install "bin/aws_bash_completer"
+    zsh_completion.install "bin/aws_zsh_completer.sh"
+    (zsh_completion/"_aws").write <<~EOS
+      #compdef aws
+      _aws () {
+        local e
+        e=$(dirname ${funcsourcetrace[1]%:*})/aws_zsh_completer.sh
+        if [[ -f $e ]]; then source $e; fi
+      }
+    EOS
+
+    system libexec/"bin/python3", "scripts/gen-ac-index", "--include-builtin-index"
+  end
+
+  def caveats
+    <<~EOS
+      The "examples" directory has been installed to:
+        #{HOMEBREW_PREFIX}/share/awscli/examples
+    EOS
+  end
+
+  test do
+    assert_match "topics", shell_output("#{bin}/aws help")
+    assert_includes Dir["#{libexec}/lib/python3.9/site-packages/awscli/data/*"],
+                    "#{libexec}/lib/python3.9/site-packages/awscli/data/ac.index"
+  end
+end
